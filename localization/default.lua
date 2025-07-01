94a79071858df8e64f4cda93291f58efd7e57751
Finnaware <78937350+Finnaware@users.noreply.github.com>
2025-07-01 06:57:27 -0300

    1.0.0

---
+++ localization/default.lua
@@ -0,0 +1,469 @@
+return {
+    descriptions = {
+        alt_texture = {
+            alt_tex_KYU_jokers = {
+                j_8_ball = {
+                    name = "8 ball",
+                },
+                j_abstract = {
+                    name = "Abstraction",
+                },
+                j_acrobat = {
+                    name = "Acrobat",
+                },
+                j_ancient = {
+                    name = "Ancient Phox",
+                },
+                j_arrowhead = {
+                    name = "Arrowhead",
+                },
+                j_astronomer = {
+                    name = "Astronomer",
+                },
+                j_banner = {
+                    name = "Banner",
+                },
+                j_baron = {
+                    name = "Baron",
+                },
+                j_baseball = {
+                    name = "Delphox's Card",
+                },
+                j_blackboard = {
+                    name = "Blackboard",
+                },
+                j_bloodstone = {
+                    name = "Bloodstone",
+                },
+                j_blue_joker = {
+                    name = "Blue Phox",
+                },
+                j_blueprint = {
+                    name = "Blueprint",
+                },
+                j_bootstraps = {
+                    name = "Bootstraps",
+                },
+                j_brainstorm = {
+                    name = "Phoxstorm",
+                },
+                j_bull = {
+                    name = "Bull",
+                },
+                j_burglar = {
+                    name = "Burglar",
+                },
+                j_burnt = {
+                    name = "Burnt Phox",
+                },
+                j_business = {
+                    name = "Business Card",
+                },
+                j_caino = {
+                    name = "Caino",
+                },
+                j_campfire = {
+                    name = "Campfire",
+                },
+                j_card_sharp = {
+                    name = "Card Sharp",
+                },
+                j_cartomancer = {
+                    name = "Cartomancer",
+                },
+                j_castle = {
+                    name = "Castle",
+                },
+                j_cavendish = {
+                    name = "Cavendish",
+                },
+                j_ceremonial = {
+                    name = "Ceremonial Dagger",
+                },
+                j_certificate = {
+                    name = "Certificate",
+                },
+                j_chaos = {
+                    name = "Chaos the Phox",
+                },
+                j_chicot = {
+                    name = "Chicot",
+                },
+                j_clever = {
+                    name = "Clever Phox",
+                },
+                j_cloud_9 = {
+                    name = "Cloud 9",
+                },
+                j_constellation = {
+                    name = "Constellation",
+                },
+                j_crafty = {
+                    name = "Crafty Phox",
+                },
+                j_crazy = {
+                    name = "Crazy Phox",
+                },
+                j_credit_card = {
+                    name = "Credit Card",
+                },
+                j_delayed_grat = {
+                    name = "Delayed Gratification",
+                },
+                j_devious = {
+                    name = "Devious Phox",
+                },
+                j_diet_cola = {
+                    name = "Soda Phox",
+                },
+                j_dna = {
+                    name = "DNA",
+                },
+                j_drivers_license = {
+                    name = "Driver's License",
+                },
+                j_droll = {
+                    name = "Droll Phox",
+                },
+                j_drunkard = {
+                    name = "Drunkard",
+                },
+                j_duo = {
+                    name = "The Duo",
+                },
+                j_dusk = {
+                    name = "Dusk",
+                },
+                j_egg = {
+                    name = "Egg",
+                },
+                j_erosion = {
+                    name = "Erosion",
+                },
+                j_even_steven = {
+                    name = "Even Steven",
+                },
+                j_faceless = {
+                    name = "Faceless Phox",
+                },
+                j_family = {
+                    name = "The Family",
+                },
+                j_fibonacci = {
+                    name = "Phoxnacci",
+                },
+                j_flash = {
+                    name = "Flash Card",
+                },
+                j_flower_pot = {
+                    name = "Flower Pot",
+                },
+                j_fortune_teller = {
+                    name = "Fortune Teller",
+                },
+                j_four_fingers = {
+                    name = "Four Fingers",
+                },
+                j_gift = {
+                    name = "Gift Card",
+                },
+                j_glass = {
+                    name = "Glass Phox",
+                },
+                j_gluttenous_joker = {
+                    name = "Gluttenous Phox",
+                },
+                j_golden = {
+                    name = "Golden Phox",
+                },
+                j_greedy_joker = {
+                    name = "Greedy Phox",
+                },
+                j_green_joker = {
+                    name = "Green Phox",
+                },
+                j_gros_michel = {
+                    name = "Gros Michel",
+                },
+                j_hack = {
+                    name = "Hack",
+                },
+                j_half = {
+                    name = "Half Phox",
+                },
+                j_hallucination = {
+                    name = "Hallucination",
+                },
+                j_hanging_chad = {
+                    name = "Hanging Chad",
+                },
+                j_hiker = {
+                    name = "Hiker",
+                },
+                j_hit_the_road = {
+                    name = "Hit the Road",
+                },
+                j_hologram = {
+                    name = "Hologram",
+                },
+                j_ice_cream = {
+                    name = "Ice Cream",
+                },
+                j_idol = {
+                    name = "The Idol",
+                },
+                j_invisible = {
+                    name = "Inivisible Phox",
+                },
+                j_joker = {
+                    name = "Phox",
+                },
+                j_jolly = {
+                    name = "Jolly Phox",
+                },
+                j_juggler = {
+                    name = "Juggler",
+                },
+                j_loyalty_card = {
+                    name = "Loyalty Card",
+                },
+                j_luchador = {
+                    name = "Luchador",
+                },
+                j_lucky_cat = {
+                    name = "Lucky Phox",
+                },
+                j_lusty_joker = {
+                    name = "Lusty Phox",
+                },
+                j_mad = {
+                    name = "Mad Phox",
+                },
+                j_madness = {
+                    name = "Madness",
+                },
+                j_mail = {
+                    name = "Mail-In Rebate",
+                },
+                j_marble = {
+                    name = "Marble Phox",
+                },
+                j_matador = {
+                    name = "Matador",
+                },
+                j_merry_andy = {
+                    name = "Merry Braixen",
+                },
+                j_midas_mask = {
+                    name = "Midas Mask",
+                },
+                j_mime = {
+                    name = "Mime",
+                },
+                j_misprint = {
+                    name = "Misprint",
+                },
+                j_mr_bones = {
+                    name = "Mr. Phox",
+                },
+                j_mystic_summit = {
+                    name = "Mystic summit",
+                },
+                j_obelisk = {
+                    name = "Obelisk",
+                },
+                j_odd_todd = {
+                    name = "Odd Todd",
+                },
+                j_onyx_agate = {
+                    name = "Onyx Agate",
+                },
+                j_oops = {
+                    name = "Oops! All Phoxes",
+                },
+                j_order = {
+                    name = "The Order",
+                },
+                j_pareidolia = {
+                    name = "Pareidolia",
+                },
+                j_perkeo = {
+                    name = "Perkeo",
+                },
+                j_photograph = {
+                    name = "Photograph",
+                },
+                j_popcorn = {
+                    name = "Popcorn",
+                },
+                j_raised_fist = {
+                    name = "Raised Paw",
+                },
+                j_ramen = {
+                    name = "Ramen",
+                },
+                j_red_card = {
+                    name = "Red Card",
+                },
+                j_reserved_parking = {
+                    name = "Reserved Parking",
+                },
+                j_ride_the_bus = {
+                    name = "Ride the Bus",
+                },
+                j_riff_raff = {
+                    name = "Riff-Raff",
+                },
+                j_ring_master = {
+                    name = "Showman",
+                },
+                j_rocket = {
+                    name = "Rocket",
+                },
+                j_rough_gem = {
+                    name = "Rough Gem",
+                },
+                j_runner = {
+                    name = "Runner",
+                },
+                j_satellite = {
+                    name = "Satellite",
+                },
+                j_scary_face = {
+                    name = "Scary Face",
+                },
+                j_scholar = {
+                    name = "Scholar",
+                },
+                j_seance = {
+                    name = "Séance",
+                },
+                j_seeing_double = {
+                    name = "Seeing Double",
+                },
+                j_selzer = {
+                    name = "Seltzer",
+                },
+                j_shoot_the_moon = {
+                    name = "Shoot the Moon",
+                },
+                j_shortcut = {
+                    name = "Shortcut",
+                },
+                j_sixth_sense = {
+                    name = "Sixth Sense",
+                },
+                j_sly = {
+                    name = "Sly Phox",
+                },
+                j_smeared = {
+                    name = "Smeared Phox",
+                },
+                j_smiley = {
+                    name = "Smiley Face",
+                },
+                j_sock_and_buskin = {
+                    name = "Sock and Buskin",
+                },
+                j_space = {
+                    name = "Space Phox",
+                },
+                j_splash = {
+                    name = "Splash",
+                },
+                j_square = {
+                    name = "Square Phox",
+                },
+                j_steel_joker = {
+                    name = "Steel Phox",
+                },
+                j_stencil = {
+                    name = "Stencil",
+                },
+                j_stone = {
+                    name = "Stone Phox",
+                },
+                j_stuntman = {
+                    name = "Stunt Phox",
+                },
+                j_supernova = {
+                    name = "Supernova",
+                },
+                j_superposition = {
+                    name = "Superposition",
+                },
+                j_swashbuckler = {
+                    name = "Swashbuckler",
+                },
+                j_throwback = {
+                    name = "Throwback",
+                },
+                j_ticket = {
+                    name = "Golden Ticket",
+                },
+                j_to_the_moon = {
+                    name = "To the Moon",
+                },
+                j_todo_list = {
+                    name = "To Do List",
+                },
+                j_trading = {
+                    name = "Trading Card",
+                },
+                j_tribe = {
+                    name = "The Tribe",
+                },
+                j_triboulet = {
+                    name = "Triboulet",
+                },
+                j_trio = {
+                    name = "The Trio",
+                },
+                j_troubadour = {
+                    name = "Troubadour",
+                },
+                j_trousers = {
+                    name = "Lost Trousers",
+                },
+                j_turtle_bean = {
+                    name = "Rawst Berry",
+                },
+                j_vagabond = {
+                    name = "Vagabond",
+                },
+                j_vampire = {
+                    name = "Vampire",
+                },
+                j_walkie_talkie = {
+                    name = "Walkie Talkie",
+                },
+                j_wee = {
+                    name = "Kyu Joker",
+                },
+                j_wily = {
+                    name = "Wily Phox",
+                },
+                j_wrathful_joker = {
+                    name = "Wrathful Phox",
+                },
+                j_yorick = {
+                    name = "Yorick"
+                },
+                j_zany = {
+                    name = "Zany Phox",
+                },
+                name = "Jokers"
+            },
+        },
+        texture_packs = {
+            texpack_KYU_cards = {
+                name = "Kyu-latro!",
+                text = {
+                    "Retextures the game",
+                    "to be Fennekin line themed!",
+                    "Made by {C:dark_edition}Finnaware"
+                }
+            }
+        }
+    }
+}
